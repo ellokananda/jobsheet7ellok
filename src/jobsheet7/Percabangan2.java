2834b0b703a793017499099900a768208736fa29
User <User@DESKTOP-HU3854Q>
2018-08-03 18:02:11 +0700

    Initial Commit

---
+++ src/jobsheet7/Percabangan2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author User
+ */
+public class Percabangan2 {
+    public static void main (String[] args) {
+    String identitas = "ellok ananda madya pratiwi / x rpl 5 / 13";
+    System.out.println("Identitas : "+ identitas);
+    
+    
+   
+    
+    
+}

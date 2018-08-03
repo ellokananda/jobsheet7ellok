7a318baf1c5965d549071992fce1739705de9829
User <User@DESKTOP-HU3854Q>
2018-08-03 10:55:26 +0700

    Coba Operator Ternary

--- src/jobsheet7/CobaPercabangan.java
+++ src/jobsheet7/CobaPercabangan.java
@@ -74,6 +74,14 @@ public class CobaPercabangan {
              case 5: System.out.println("nilai a=>5"); break;
              default: System.out.println("nilai a bukan antara 1-5");
              
+             int nilai;
+             System.out.print("\nNilai Anda? ");
+             nilai=scanner.nextInt();
+             
+             String grade = (nilai<75) ? "belum kompeten" : "kompeten";
+             System.out.println(nilai + " => " + grade);
+             
+             
          }
              
          }

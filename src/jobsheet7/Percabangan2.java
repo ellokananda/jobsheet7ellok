79e3454a6feb4ff406618cda565ef09e1cf32374
User <User@DESKTOP-HU3854Q>
2018-08-03 18:14:53 +0700

    Tambah Input Bahan 2

--- src/jobsheet7/Percabangan2.java
+++ src/jobsheet7/Percabangan2.java
@@ -24,6 +24,34 @@ public class Percabangan2 {
     System.out.println("Masukkan no pilihan Anda");
     int bahan1 = scanner.nextInt();
     
+    if(bahan1== 1 || bahan1 == 2){
+        System.out.println("Bahan Kedua:");
+        if(bahan1 == 1){
+            System.out.println("1.Susu");
+            System.out.println("2.Minyak Goreng");
+            System.out.println("3.Tidak ada");
+        }else{
+            System.out.println("1.Minyak Goreng");
+            System.out.println("2.Roti");
+            System.out.println("3.Tidak ada");
+        }
+        System.out.print("Masukkan no pilihan Anda");
+        int bahan2 = scanner.nextInt();
+        
+    }else
+        System.out.println("Mohon maaf, Pilihan tidak ditemukan,"
+                + "tidak dapat memberikan saran resep");
+    
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
     
     
     
@@ -31,5 +59,4 @@ public class Percabangan2 {
    
     
     
-}
 }

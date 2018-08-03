9b8ee79774e457ef92a9ae5f830aac58506e58a6
User <User@DESKTOP-HU3854Q>
2018-08-03 13:22:03 +0700

    Coba IF

--- src/jobsheet7/LatihanPercabangan.java
+++ src/jobsheet7/LatihanPercabangan.java
@@ -5,6 +5,8 @@
  */
 package jobsheet7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author User
@@ -13,7 +15,28 @@ public class LatihanPercabangan {
     public static void main(String[] args){
         String identitas = " ellok ananda madya pratiwi / x rpl 5/ 13";
         System.out.println("Identitas :" + identitas);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if (a<5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if (a==5) System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if (a>5) {
+            System.out.println("isi variabel a : "+ a);
+            System.out.println("nilai a lebih dari 5");
+            
+            
+        }
     }
 }
+        
+    
+    
+
         
 

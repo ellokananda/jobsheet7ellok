14fc1c7aa61b20e75cae29c3ad8cbb8e2a3cf54c
User <User@DESKTOP-HU3854Q>
2018-08-03 13:12:28 +0700

    Initial Commit

---
+++ src/jobsheet7/LatihanPercabangan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+
+/**
+ *
+ * @author User
+ */
+public class LatihanPercabangan {
+    public static void main(String[] args){
+        String identitas = " ellok ananda madya pratiwi / x rpl 5/ 13";
+        System.out.println("Identitas :" + identitas);
+    }
+}
+        
+
